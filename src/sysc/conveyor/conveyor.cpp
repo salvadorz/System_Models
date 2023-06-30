f0def3dc4218c1e87d3dfe0a55ab25c33dddfa45
Salvador Z <salvador.zendejas@gmail.com>
2023-06-30 13:23:48 -0500

    [F001] - Update Conveyor System

--- src/sysc/conveyor/conveyor.cpp
+++ src/sysc/conveyor/conveyor.cpp
@@ -23,6 +23,7 @@
  ******************************************************************************/
 
 #include "conveyor.hpp"
+#include "HashMap.hpp"
 #include <systemc.h>
 
 /**
@@ -31,14 +32,14 @@
  * conveyor belt. It sends a scanner status packet to the control system using a fifo
  * @param Input control_packet from control system
  * @param Output scanner_sts_packet to control system
- * 
+ *
  */
 class scanner : public sc_module {
 private:
   int                 bag_id;
   int                 running;
   int                 var_delay;
-  int                 slots_available;
+  int                 samples_available;
   control_packet     *control_pkt_ptr;
   scanner_sts_packet *scanner_pkt_ptr;
 
@@ -52,7 +53,7 @@ public:
     // process declaration
     SC_THREAD(scanner_thread);
 
-    bag_id = 0;
+    bag_id  = 0;
     running = 0; // off initially
   }
 
@@ -73,9 +74,9 @@ public:
       /**
        * Check for received control packets
        */
-      slots_available = in->num_available();
+      samples_available = in->num_available();
 
-      if (slots_available != 0) {
+      if (samples_available != 0) {
         in->read(control_pkt_ptr);
 
         if (control_pkt_ptr->get_msg() == CONTROL_PKT_MSG_TURN_ON) {
@@ -103,15 +104,165 @@ public:
         if (0 > bag_id) {
           bag_id = 0;
         }
+        out->write(scanner_pkt_ptr);
       }
     } // end while
   }   // end scanner_thread
 };
 
+/**
+ * @brief Control_system module
+ * This module controls the conveyor belt. It communicates with the scanner and the conveyor through sc_fifos
+ * @param Input scanner_sts_packet from scanner
+ * @param Input conveyor_sts_packet from conveyor system
+ * @param Output control_packet to scanner system
+ * @param Output control_packet to conveyor system
+ */
+class Control_System : public sc_module {
+private:
+  int index;
+  int bag_id;
+  int bag_count;
+  int scanner_running;
+  int samples_available;
+  int control_system_loop_count;
 
+  // Control packets
+  control_packet *control_pkt_ptr;
 
-  int sc_main(int, char *[]) {
+  // Received status packets
+  scanner_sts_packet  *scanner_pkt_ptr;
+  conveyor_sts_packet *conveyor_pkt_ptr;
 
-    sc_start();
-    return 0;
+  // arrays that hold the pointers to the sc_ports
+  sc_port<sc_fifo_in_if<conveyor_sts_packet *> > *seg_in_port[NUM_CONVEYOR_SEGMENTS];
+  sc_port<sc_fifo_out_if<control_packet *> >     *seg_out_port[NUM_CONVEYOR_SEGMENTS];
+
+  ds::HashMap bag_hash; // hash table to keep track of BagID's
+
+public:
+  // port list
+  sc_port<sc_fifo_in_if<scanner_sts_packet *> > scanner_in;
+  sc_port<sc_fifo_out_if<control_packet *> >    scanner_out;
+
+  sc_port<sc_fifo_in_if<conveyor_sts_packet *> > seg0_in;
+  sc_port<sc_fifo_out_if<control_packet *> >     seg0_out;
+
+  SC_HAS_PROCESS(Control_System);
+
+  Control_System(sc_module_name name, int csl_count) : sc_module(name), control_system_loop_count(csl_count), bag_hash(true, 128) {
+    // process declaration
+    SC_THREAD(control_system_thread);
+
+    // initialize variables
+    index             = 0;
+    bag_id            = 0;
+    bag_count         = 0;
+    scanner_running   = 0;
+    samples_available = 0;
+
+    // initialize ports
+    seg_in_port[0]  = &seg0_in;
+    seg_out_port[0] = &seg0_out;
+    //bag_hash        = new ds::HashMap(true, 128);
   }
+
+  void control_system_thread() {
+
+    control_pkt_ptr = new control_packet();
+
+    control_pkt_ptr->set_timestamp(sc_time_stamp());
+    control_pkt_ptr->set_msg(CONTROL_PKT_MSG_TURN_ON);
+    control_pkt_ptr->set_data(0);
+
+    scanner_out->write(control_pkt_ptr);
+    scanner_running = CONTROL_PKT_MSG_TURN_ON;
+
+    // Cycle through the conveyor segments
+    for (index = 0; index < NUM_CONVEYOR_SEGMENTS; index++) {
+      control_pkt_ptr = new control_packet();
+
+      control_pkt_ptr->set_timestamp(sc_time_stamp());
+      control_pkt_ptr->set_msg(CONTROL_PKT_MSG_TURN_ON);
+      control_pkt_ptr->set_data(0);
+
+      (*seg_out_port[index])->write(control_pkt_ptr); // send it to conveyor segment
+    }
+
+    while (true) {
+      wait(CONTROL_SYSTEM_RATE_US, SC_US);
+
+      // Check for received scanner status packets
+      samples_available = scanner_in->num_available();
+      if (samples_available != 0) {
+        scanner_in->read(scanner_pkt_ptr);
+
+        // save the scanner_pkt_ptr in the bag_hash
+        bag_hash.Insert(scanner_pkt_ptr->get_bag_id(), scanner_pkt_ptr);
+
+        ++bag_count;
+        if ((scanner_running == CONTROL_PKT_MSG_TURN_ON) && (bag_count >= MAX_NUMBER_BAGS_IN_SYSTEM)) {
+
+          // send a turn off command to the scanner
+          control_pkt_ptr = new control_packet();
+
+          control_pkt_ptr->set_timestamp(sc_time_stamp());
+          control_pkt_ptr->set_msg(CONTROL_PKT_MSG_TURN_OFF);
+          control_pkt_ptr->set_data(0);
+
+          scanner_out->write(control_pkt_ptr);
+
+          scanner_running = CONTROL_PKT_MSG_TURN_OFF;
+        }
+      } // end if samples_available
+
+      // Check for received conveyor status packets
+      samples_available = seg0_in->num_available();
+      if (samples_available != 0) {
+        seg0_in->read(conveyor_pkt_ptr);
+
+        conveyor_pkt_ptr->print();
+
+        // Check for alarm condition
+
+        // Update bag postiiion and delete bags delivered
+        bag_id = bag_hash.GetFirstKey();
+        if (-1 != bag_id) {
+          scanner_pkt_ptr = (scanner_sts_packet *)bag_hash.GetValue(bag_id);
+        }
+
+        if ((CONTROL_PKT_MSG_TURN_OFF == scanner_running) &&
+            (bag_count < (MAX_NUMBER_BAGS_IN_SYSTEM - BAG_COUNT_HYSTERESIS))) {
+
+          // send a turn on command to the scanner
+          control_pkt_ptr = new control_packet();
+
+          control_pkt_ptr->set_timestamp(sc_time_stamp());
+          control_pkt_ptr->set_msg(CONTROL_PKT_MSG_TURN_ON);
+          control_pkt_ptr->set_data(0);
+
+          scanner_out->write(control_pkt_ptr);
+
+          scanner_running = CONTROL_PKT_MSG_TURN_ON;
+        }
+        delete conveyor_pkt_ptr;
+      }
+
+      --control_system_loop_count;
+      if (0 == control_system_loop_count) {
+        break;
+      }
+    } // end while
+      // stop
+    cout.flush();
+    sc_stop();
+  } // end control_system_thread
+  ~Control_System() {
+  }
+};
+
+int sc_main(int, char *[]) {
+
+  sc_start();
+  return 0;
+}
